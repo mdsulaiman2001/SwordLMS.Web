a64db602afab26a27c1b14a25488557dd049f72d
mohamed.sulaiman <mohamed.sulaiman@SWPC177>
2023-03-06 19:49:26 +0530

    6/03/23.

--- Models/SwordLmsContext.cs
+++ Models/SwordLmsContext.cs
@@ -50,7 +50,6 @@ public partial class SwordLmsContext : DbContext
     public virtual DbSet<UserCourse> UserCourses { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
         => optionsBuilder.UseSqlServer("Server=(LocalDB)\\MSSQLLocalDB;Database=SwordLMS;Trusted_Connection=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
