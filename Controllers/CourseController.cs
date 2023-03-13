cba8c55bfc6d56c1ca2cf1174eba1a0f91b96041
mohamed.sulaiman <mohamed.sulaiman@SWPC177>
2023-03-13 20:17:28 +0530

    13/03/2023 commit

---
+++ Controllers/CourseController.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using SwordLMS.Web.Models;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
+
+namespace SwordLMS.Web.Controllers
+{
+    public class CourseController : Controller
+    {
+
+        private readonly SwordLmsContext _context;
+
+        // private readonly CourseViewModel _courseViewModel;
+
+        CourseViewModel courseViewModel= new CourseViewModel();
+       
+       
+
+
+
+        public CourseController(SwordLmsContext context /*CourseViewModel courseViewModel*/)
+        {
+            _context = context;
+            //_courseViewModel = courseViewModel;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            
+
+            return View();
+        }
+
+        public async Task<IActionResult> SaveCourse(Course course)
+        {
+            var dateTime = DateTime.Now.ToShortDateString();
+            course.DateOfPublish = Convert.ToDateTime(dateTime);
+            _context.Courses.Add(course);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Create");
+
+        }
+
+        public async Task<IActionResult> SaveContent(CourseContent courseContent)
+        {
+            if (ModelState.IsValid) 
+            {
+                _context.CourseContents.Add(courseContent); 
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Create"); 
+
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> SaveTopics(CourseTopic courseTopic)
+        {
+            if(ModelState.IsValid)
+            {
+                _context.CourseTopics.Add(courseTopic);
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction("Create");
+            }
+            return View();  
+        }
+         public async Task<IActionResult> SaveSkills(CourseSkill courseSkill)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.CourseSkills.Add(courseSkill);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Create");
+            }
+            return View();
+
+        }
+    } 
+}
---
+++ Models/CourseViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SwordLMS.Web.Models
+{
+    public class CourseViewModel
+    {
+        public Course courseModel { get; set; }
+        public CourseSkill courseSkillModel { get; set; }
+        public CourseContent courseContentModel { get; set; }
+        public CourseTopic courseTopicModel { get; set; }
+
+    }
+}
