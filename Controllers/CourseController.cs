aef0b58eb37fd43de4ca6987fdf730b014b28736
jegan <jegan@happzo.com>
2023-03-20 01:03:18 +0530

    Save course complted with image upload

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -15,8 +15,8 @@ namespace SwordLMS.Web.Controllers
     public class CourseController : Controller
     {
 
-     
 
+        private readonly IWebHostEnvironment _hostingEnvironment;
         private readonly SwordLmsContext _context;
 
         // private readonly CourseViewModel _courseViewModel;
@@ -27,9 +27,10 @@ namespace SwordLMS.Web.Controllers
 
 
 
-        public CourseController(SwordLmsContext context /*CourseViewModel courseViewModel*/)
+        public CourseController(SwordLmsContext context /*CourseViewModel courseViewModel*/, IWebHostEnvironment hostingEnvironment)
         {
             _context = context;
+            _hostingEnvironment = hostingEnvironment;
             //_courseViewModel = courseViewModel;
         }
 
@@ -73,15 +74,54 @@ namespace SwordLMS.Web.Controllers
             return Json(new { success = true });
 
         }
-        public async Task<IActionResult> SaveCourse([FromBody]Course course)
+        public async Task<IActionResult> SaveCourse(IFormFile file, [FromForm]string data)
         {
-            // var dateTime = DateTime.Now.ToShortDateString();
-            // course.DateOfPublish = Convert.ToDateTime(dateTime);
-            course.DisplayImagePath = "";
-           _context.Courses.Add(course);
-           _context.SaveChanges();
-            return Json(course.Id);
-           // return RedirectToAction("Create");
+            string filePath = string.Empty;
+            Course course=null;
+            try {
+                // var dateTime = DateTime.Now.ToShortDateString();
+                // course.DateOfPublish = Convert.ToDateTime(dateTime);
+                // course.DisplayImagePath = "";
+                if (file != null && file.Length > 0 &&  data != null)
+                {
+                    string fileName = Path.GetFileName(file.FileName);
+                    string fileExtension = Path.GetExtension(fileName);
+
+                    string newFileName = Guid.NewGuid().ToString() + fileExtension;
+
+                    filePath = Path.Combine(_hostingEnvironment.WebRootPath, "CourseContents", newFileName);
+
+                    using (var stream = new FileStream(filePath, FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+               
+               
+                course = JsonConvert.DeserializeObject<Course>(data);
+                if (course is not null) {
+                    course.DisplayImagePath = filePath;
+                    _context.Courses.Add(course);
+                    _context.SaveChanges();
+                    return Json(course.Id);
+                }
+                else
+                {
+                    if (course.Id == null)
+                    {
+                        System.IO.File.Delete(filePath);
+                    }
+                    return null;
+                }
+                // return RedirectToAction("Create");
+            }
+            catch (Exception ex) {
+                if(course.Id==null)
+                {
+                    System.IO.File.Delete(filePath);
+                }
+                return null;
+            }
 
         }
 
