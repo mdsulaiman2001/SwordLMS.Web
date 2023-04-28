413b0b60aee4e839b7c025626592696a9b36ee96
mohamed.sulaiman <mohamed.sulaiman@SWPC177>
2023-04-28 16:24:00 +0530

    validation are added

--- Models/User.cs
+++ Models/User.cs
@@ -18,9 +18,12 @@ public partial class User
 
     public string LastName { get; set; }
 
-    [Required(ErrorMessage = "Please Enter Your Email"), MaxLength(50)]
 
-    [RegularExpression("^([0-9]{10})$", ErrorMessage = "Invalid Mobile Number.")]
+    [Required(ErrorMessage = "Please Enter Your Email"), MaxLength(50)]
+    [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}" +
+                            @"\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\" +
+                            @".)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$",
+                            ErrorMessage = "Email is not valid")]
     public string Email { get; set; } = null!;
 
     [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
